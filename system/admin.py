a96d20c8460c550c33683d5399f98235636699c3
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-29 18:06:57 +0200

    Register user model in admin

--- system/admin.py
+++ system/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from account.models import *
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone_number', 'is_active', 'is_staff')
+    search_fields = ('name', 'email', 'phone_number')
