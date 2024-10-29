f59db1d6ff13d679869b4220d707af6ee332a7c8
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-29 15:11:16 +0200

    Add permission routes in system app

--- system/urls.py
+++ system/urls.py
@@ -6,5 +6,8 @@ from django.conf.urls.static import static
 app_name = 'system'
 
 urlpatterns = [
-    # 
+    path('permissions/', PermissionListView.as_view(), name='permissionList'),
+    path('assign-permission/', AssignPermissionView.as_view(), name='assignPermission'),
+    path('remove-permission/', RemovePermissionView.as_view(), name='removePermission'),
+    path('permissions/<int:user_id>/', UserPermissionsView.as_view(), name='userPermissions'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
