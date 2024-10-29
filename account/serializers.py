24cca525879d82c167df9bcf619e24008e758357
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-29 14:53:21 +0200

    Add UserSerializer and PermissionSerializer

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,33 @@
 from account.models import *
 from rest_framework import serializers
+from django.contrib.auth.models import Permission
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True, required=True)
+
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        fields = ('id', 'name', 'codename', 'content_type')
+
+class UserSerializer(serializers.ModelSerializer):
+    user_permissions = serializers.SerializerMethodField()
+    class Meta:
+        model = User
+        fields = (
+            'id', 'name', 'email', 'phone_number', 'role', 'password', 'user_permissions'
+        )
+
+    def get_user_permissions(self, obj):
+        """Retrieve the user's direct permissions."""
+        return obj.get_all_permissions()
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User.objects.create_user(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+
+        return user
