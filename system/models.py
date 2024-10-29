041203757dab64e790a34d5d93b5bfefad98a31a
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-29 15:52:11 +0200

    New changes

--- system/models.py
+++ system/models.py
@@ -4,7 +4,7 @@ class Rider(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
     address = models.CharField(max_length=20, null=True, blank=True)
-    tag_id = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    code = models.CharField(max_length=20, unique=True, null=True, blank=True)
     nid = models.CharField(max_length=20, unique=True, null=True, blank=True)
 
     def __str__(self):
