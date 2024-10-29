62454464604da661f955564881d9effca6698d65
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-29 15:50:55 +0200

    Create riders model

--- system/models.py
+++ system/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Rider(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    address = models.CharField(max_length=20, null=True, blank=True)
+    tag_id = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    nid = models.CharField(max_length=20, unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
