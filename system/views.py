fffc8d90fbdbe75a69139e8a2083ed21b62dd3b7
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-29 15:12:57 +0200

    Add function to retrieve all permissions

--- system/views.py
+++ system/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from account.serializers import *
+from django.db import transaction
+from django.db.models import Count, Q
+from rest_framework.views import APIView
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.core.exceptions import PermissionDenied
+from rest_framework import generics, permissions, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+class PermissionListView(generics.ListAPIView):
+    """
+    View to list all permissions. Accessible only to users with appropriate permissions or superusers.
+    """
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
+    permission_classes = [permissions.IsAuthenticated] # Change to ensure only authenticated users can access
+
+    def get_queryset(self):
+        # Allow superusers unrestricted access
+        if self.request.user.is_superuser:
+            return super().get_queryset()
+
+        # Check if the user has the 'view_permission' permission
+        if not self.request.user.has_perm('auth.view_permission'):
+            raise PermissionDenied({'message': "You do not have permission to view permissions."})
+
+        return super().get_queryset()
