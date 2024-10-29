cde18f9d6c4b1a48f3faa350c74d4286b2a04c3d
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-29 15:07:22 +0200

    Setup system app route

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('system.urls')),
     path('api/auth/', include('account.urls')),
 ]
