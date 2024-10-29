7ede5ed6295a64636afa8b29f7c302649760aa11
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-29 14:46:54 +0200

    Setup account routes

---
+++ account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf import settings
+from account.views import *
+from django.conf.urls.static import static
+
+app_name = 'auth'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile-update/', UpdateUserView.as_view(), name='update'),
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
