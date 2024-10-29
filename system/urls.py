d8eb22536955e05d7961e5d06129b81cefb02064
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-29 15:17:41 +0200

    Add route for CRUD user

--- system/urls.py
+++ system/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('assign-permission/', AssignPermissionView.as_view(), name='assignPermission'),
     path('remove-permission/', RemovePermissionView.as_view(), name='removePermission'),
     path('permissions/<int:user_id>/', UserPermissionsView.as_view(), name='userPermissions'),
+
+    path('users/', UserListView.as_view(), name='getUsers'),
+    path('user/<int:user_id>/', UserDetailView.as_view(), name='userDetail'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
