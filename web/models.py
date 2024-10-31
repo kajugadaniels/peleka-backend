5e8624809509930670290cd31ff5f568f4f14e01
Munyeshya <obedbenny54@gmail.com>
2024-10-31 12:04:43 +0200

    Created Request Demo table

--- web/models.py
+++ web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class RequestDemo(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    company_name = models.CharField(max_length=100, null=True, blank=True)
+    contact_number = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    message = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.company_name}"
+
