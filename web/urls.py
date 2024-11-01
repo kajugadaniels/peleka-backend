c29cbe0a96681857cd48bb1cf315630fec889549
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-01 12:52:56 +0200

    Create endpoint to retrieve riders for web

---
+++ web/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from web.views import *
+from django.conf.urls.static import static
+
+app_name = 'web'
+
+urlpatterns = [
+    path('riders/', RiderListView.as_view(), name='getRiders'),
+    path('riders/<int:pk>/', RiderDetailView.as_view(), name='getRiderDetails'),
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- web/views.py
+++ web/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from system.models import *
+from system.serializers import *
+from account.serializers import *
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class RiderListView(generics.ListAPIView):
+    """
+    API view to list all Riders.
+    Accessible to anyone.
+    """
+    queryset = Rider.objects.all()
+    serializer_class = RiderSerializer
+    permission_classes = [AllowAny]  # Allow any user to access this view
+
+class RiderDetailView(generics.RetrieveAPIView):
+    """
+    API view to retrieve Rider details.
+    Accessible to anyone.
+    """
+    queryset = Rider.objects.all()
+    serializer_class = RiderSerializer
+    permission_classes = [AllowAny]  # Allow any user to access this view
