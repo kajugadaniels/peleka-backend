14051e50a0544a30707b0b59f20cf3600f648eb4
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-02 18:19:21 +0200

    New changes

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('system.urls')),
     path('api/web/', include('web.urls')),
     path('api/auth/', include('account.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
