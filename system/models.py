94e4e2ed669f3a3739735d0d2cfc10d9f1661dbf
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-02 17:29:56 +0200

    Add image field in Rider model

--- system/models.py
+++ system/models.py
@@ -1,4 +1,13 @@
+import os
 from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+def rider_image_path(instance, filename):
+    base_filename, file_extension = os.path.splitext(filename)
+    return f'riders/rider_{slugify(instance.name)}_{instance.phone_number}_{instance.code}{file_extension}'
 
 class Rider(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
@@ -6,6 +15,16 @@ class Rider(models.Model):
     address = models.CharField(max_length=20, null=True, blank=True)
     code = models.CharField(max_length=20, unique=True, null=True, blank=True)
     nid = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    image = ProcessedImageField(
+        upload_to=rider_image_path,
+        processors=[ResizeToFill(1270, 1270)],
+        format='JPEG',
+        options={'quality': 90},
+        null=True,
+        blank=True
+    )
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
