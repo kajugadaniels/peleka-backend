0affd1a657f53aad927907108873e00076b4b542
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-04 11:15:17 +0200

    New changes

--- account/serializers.py
+++ account/serializers.py
@@ -1,3 +1,4 @@
+from account.models import *
 from rest_framework import serializers
 from django.contrib.auth.models import Permission
 
@@ -19,3 +20,57 @@ class PermissionSerializer(serializers.ModelSerializer):
         app_label = obj.content_type.app_label
         codename = obj.codename
         return f"{app_label}.{codename}"
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = ('id', 'name')
+
+class UserSerializer(serializers.ModelSerializer):
+    role_name = serializers.CharField(source='role.name', read_only=True)
+    user_permissions = serializers.SerializerMethodField()
+    image = serializers.ImageField(required=False)
+
+    class Meta:
+        model = User
+        fields = (
+            'id', 'name', 'email', 'phone_number', 'role', 'role_name', 'image', 'password', 'user_permissions'
+        )
+        extra_kwargs = {
+            'password': {'write_only': True}  # Password should be write-only for security
+        }
+
+    def get_user_permissions(self, obj):
+        """Retrieve the user's direct permissions."""
+        return obj.get_all_permissions()
+
+    def create(self, validated_data):
+        """
+        Create a new user with a hashed password.
+        """
+        password = validated_data.pop('password', None)
+        user = User.objects.create_user(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        """
+        Update user details and hash the password if provided.
+        """
+        # Handle image upload separately if provided
+        if 'image' in validated_data:
+            instance.image = validated_data.pop('image')
+
+        # If a password is provided, hash it using set_password
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)  # Hash and set the new password
+
+        # Update the remaining fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
--- system/serializers.py
+++ system/serializers.py
@@ -1,61 +1,6 @@
 import random
-from system.models import *
-from account.models import *
 from rest_framework import serializers
-
-class RoleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Role
-        fields = ('id', 'name')
-
-class UserSerializer(serializers.ModelSerializer):
-    role_name = serializers.CharField(source='role.name', read_only=True)
-    user_permissions = serializers.SerializerMethodField()
-    image = serializers.ImageField(required=False)
-
-    class Meta:
-        model = User
-        fields = (
-            'id', 'name', 'email', 'phone_number', 'role', 'role_name', 'image', 'password', 'user_permissions'
-        )
-        extra_kwargs = {
-            'password': {'write_only': True}  # Password should be write-only for security
-        }
-
-    def get_user_permissions(self, obj):
-        """Retrieve the user's direct permissions."""
-        return obj.get_all_permissions()
-
-    def create(self, validated_data):
-        """
-        Create a new user with a hashed password.
-        """
-        password = validated_data.pop('password', None)
-        user = User.objects.create_user(**validated_data)
-        if password:
-            user.set_password(password)
-        user.save()
-        return user
-
-    def update(self, instance, validated_data):
-        """
-        Update user details and hash the password if provided.
-        """
-        # Handle image upload separately if provided
-        if 'image' in validated_data:
-            instance.image = validated_data.pop('image')
-
-        # If a password is provided, hash it using set_password
-        password = validated_data.pop('password', None)
-        if password:
-            instance.set_password(password)  # Hash and set the new password
-
-        # Update the remaining fields
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-
-        instance.save()
-        return instance
+from system.models import *
 
 class RiderSerializer(serializers.ModelSerializer):
     """
