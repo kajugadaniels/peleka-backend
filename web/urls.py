08592969288d6f7408fa285fff12c235fa47303e
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-04 16:52:44 +0200

    Registered user-specific delivery request CRUD routes in URLs.

--- web/urls.py
+++ web/urls.py
@@ -8,4 +8,10 @@ app_name = 'web'
 urlpatterns = [
     path('riders/', RiderListView.as_view(), name='getRiders'),
     path('riders/<int:pk>/', RiderDetailView.as_view(), name='getRiderDetails'),
+
+    path('delivery-requests/', UserDeliveryRequestListView.as_view(), name='userDeliveryRequestList'),
+    path('delivery-request/', UserDeliveryRequestCreateView.as_view(), name='userDeliveryRequestCreate'),
+    path('delivery-request/<int:pk>/', UserDeliveryRequestDetailView.as_view(), name='userDeliveryRequestDetail'),
+    path('delivery-request/update/<int:pk>/', UserDeliveryRequestUpdateView.as_view(), name='userDeliveryRequestUpdate'),
+    path('delivery-request/delete/<int:pk>/', UserDeleteDeliveryRequestView.as_view(), name='userDeliveryRequestDelete'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
