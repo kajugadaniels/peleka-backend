94116f78b4f854cc0bba6539b74182539b61fc4e
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-04 16:49:10 +0200

    Added UserDeliveryRequestListView to list delivery requests for the logged-in user.

--- web/views.py
+++ web/views.py
@@ -1,8 +1,9 @@
 from system.models import *
 from system.serializers import *
 from account.serializers import *
-from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework import generics, permissions
+from rest_framework.exceptions import PermissionDenied
 
 class RiderListView(generics.ListAPIView):
     """
@@ -21,3 +22,19 @@ class RiderDetailView(generics.RetrieveAPIView):
     queryset = Rider.objects.all()
     serializer_class = RiderSerializer
     permission_classes = [AllowAny]  # Allow any user to access this view
+
+class UserDeliveryRequestListView(generics.ListAPIView):
+    """
+    API view to list all Delivery Requests for the logged-in user.
+    - Accessible only to authenticated users.
+    """
+    serializer_class = DeliveryRequestSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Only fetch delivery requests made by the logged-in user
+        user = self.request.user
+        return DeliveryRequest.objects.filter(client=user, delete_status=False).order_by('-created_at')
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
