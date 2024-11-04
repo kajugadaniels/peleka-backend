1d0fc2cca0cfcd70345869b8a1522bc1d1d9b502
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-04 14:35:26 +0200

    Add DistancePrice model

--- system/models.py
+++ system/models.py
@@ -28,3 +28,23 @@ class Rider(models.Model):
 
     def __str__(self):
         return self.name
+
+class DistancePricing(models.Model):
+    BASE_DISTANCE = 5  # kilometers
+    BASE_PRICE = 1000  # RWF
+    ADDITIONAL_DISTANCE = 5  # kilometers for each additional rate
+    ADDITIONAL_PRICE = 500  # RWF for every additional 5km
+
+    class Meta:
+        verbose_name = 'Distance Pricing'
+        verbose_name_plural = 'Distance Pricing'
+
+    @classmethod
+    def calculate_price(cls, distance_km):
+        if distance_km <= cls.BASE_DISTANCE:
+            return cls.BASE_PRICE
+        additional_distance = max(0, distance_km - cls.BASE_DISTANCE)
+        additional_blocks = additional_distance // cls.ADDITIONAL_DISTANCE
+        if additional_distance % cls.ADDITIONAL_DISTANCE > 0:
+            additional_blocks += 1
+        return cls.BASE_PRICE + (additional_blocks * cls.ADDITIONAL_PRICE)
