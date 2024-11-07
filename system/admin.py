a9018671bb25d9e110713db448bf576a2948dd3e
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-07 15:48:06 +0200

    Register DeliveryRequest in admin

--- system/admin.py
+++ system/admin.py
@@ -7,6 +7,10 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone_number', 'is_active', 'is_staff')
     search_fields = ('name', 'email', 'phone_number')
 
+@admin.register(DeliveryRequest)
+class DeliveryRequestAdmin(admin.ModelAdmin):
+    list_display = ('package_name', 'client')
+
 @admin.register(RiderDelivery)
 class RiderDeliveryAdmin(admin.ModelAdmin):
     list_display = ('rider', )
