63c3461ab1fd3178c3d93dd5508d3f3595ef3a64
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-11 17:58:53 +0200

    Add login functionality for user on web

--- web/urls.py
+++ web/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 app_name = 'web'
 
 urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register')
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- web/views.py
+++ web/views.py
@@ -1,12 +1,38 @@
 from system.models import *
 from web.serializers import *
 from web.serializers import *
-# from account.serializers import *
 from rest_framework.response import Response
+from django.contrib.auth import authenticate
 from rest_framework.exceptions import NotFound
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
+from rest_framework.generics import GenericAPIView
 from rest_framework import generics, permissions, status
+
+class LoginView(GenericAPIView):  # Change to GenericAPIView
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+
+        user = authenticate(username=email, password=password)
+
+        if user:
+            # Delete old token and generate a new one
+            Token.objects.filter(user=user).delete()
+            token, created = Token.objects.get_or_create(user=user)
+
+            return Response({
+                'token': token.key,
+                'user': UserSerializer(user).data,
+                'message': 'Login successful.'
+            }, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid email or password.'}, status=status.HTTP_400_BAD_REQUEST)
 
 class RegisterView(generics.CreateAPIView):
     """
