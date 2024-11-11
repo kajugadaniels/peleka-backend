26693cf28f45c5047d5c23989de9b5d246d0dace
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-11 18:57:56 +0200

    Add rider info to delivery request serializer

--- web/serializers.py
+++ web/serializers.py
@@ -34,6 +34,7 @@ class UserDeliveryRequestSerializer(serializers.ModelSerializer):
     client_name = serializers.ReadOnlyField(source='client.name', help_text='The name of the client who made the request')
     client_phone = serializers.ReadOnlyField(source='client.phone_number', help_text='The phone number of the client who made the request')
     delivery_price = serializers.DecimalField(max_digits=10, decimal_places=2, help_text='Automatically calculated price based on distance')
+    rider_info = serializers.SerializerMethodField(help_text='Information about the rider assigned to the delivery')
 
     class Meta:
         model = DeliveryRequest
@@ -41,13 +42,20 @@ class UserDeliveryRequestSerializer(serializers.ModelSerializer):
             'id', 'client', 'client_name', 'client_phone', 'pickup_address', 'pickup_lat', 'pickup_lng',
             'delivery_address', 'delivery_lat', 'delivery_lng', 'package_name', 'package_description',
             'recipient_name', 'recipient_phone', 'estimated_distance_km', 'estimated_delivery_time', 
-            'value_of_product', 'delivery_price', 'image', 'status', 'payment_type', 'created_at', 'updated_at'
+            'value_of_product', 'delivery_price', 'image', 'status', 'payment_type', 
+            'created_at', 'updated_at', 'rider_info'
         ]
         read_only_fields = ['id', 'created_at', 'updated_at']
         extra_kwargs = {
             'client': {'write_only': True},
             'image': {'required': False, 'allow_null': True},
         }
+
+    def get_rider_info(self, obj):
+        rider_delivery = obj.rider_assignment.first()  # Assuming a reverse relationship from DeliveryRequest to RiderDelivery
+        if rider_delivery and rider_delivery.rider:
+            return RiderSerializer(rider_delivery.rider).data
+        return None
 
     def create(self, validated_data):
         delivery_request = DeliveryRequest(**validated_data)
