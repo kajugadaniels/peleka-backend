bf63461237afed72aa274735831cac629caf00fc
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-12-02 17:25:36 +0200

    New changes

--- api/settings.py
+++ api/settings.py
@@ -203,3 +203,5 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 # DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Your App <no-reply@yourapp.com>')
 
 GOOGLE_CLIENT_ID = '165505940635-oda6u8kfjnsgnit4gqfrvpekdd15aqo7.apps.googleusercontent.com'
+
+CONTACT_EMAIL = 'kajugadaniels@gmail.com'
