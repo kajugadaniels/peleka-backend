0f933d4a7845808a36c27a581b60decd92a64df7
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-12-11 09:54:33 +0200

    Fix errors

--- web/models.py
+++ web/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class RequestDemo(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
@@ -6,16 +7,18 @@ class RequestDemo(models.Model):
     contact_number = models.CharField(max_length=20, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     message = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.name} - {self.company_name}"
 
 class ContactUs(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    subject = models.CharField(max_length=255)
-    message = models.TextField()
+    name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    subject = models.CharField(max_length=255, null=True, blank=True)
+    message = models.TextField(null=True, blank=True)
     submitted_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.name} - {self.subject}"
