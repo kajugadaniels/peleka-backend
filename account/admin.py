964a78601ee58ae032e614ddec5cc77247d1f2e6
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-04 16:57:18 +0200

    Register role model in admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,12 @@
+from account.models import *
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('name',)
+    ordering = ('-created_at',)
+    readonly_fields = ('id', 'created_at')
