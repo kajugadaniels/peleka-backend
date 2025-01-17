a724c47a0c594c11c0f5bfa08d5e760dc50b00ed
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-17 18:55:32 +0200

    Register wallets models in wallets/admin.py

--- wallets/admin.py
+++ wallets/admin.py
@@ -1,3 +1,10 @@
+from wallets.models import *
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'rider', 'rider_total', 'commissioner_total', 'boss_total', 'created_at', 'updated_at')
+
+@admin.register(TransactionHistory)
+class TransactionHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'transaction', 'role', 'amount', 'created_at')
