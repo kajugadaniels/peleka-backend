33ef00919e395389299d7ef4258dd69cbc049432
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-17 19:14:25 +0200

    New changes

--- system/models.py
+++ system/models.py
@@ -41,6 +41,7 @@ class Rider(models.Model):
         null=True,
         blank=True
     )
+    # NEW fields:
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -62,7 +63,7 @@ class Rider(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name='boss_riders',
+        related_name='commissioner_riders',
         help_text="Optional: The boss agent for this rider."
     )
     created_at = models.DateTimeField(default=timezone.now)
