2e2f7b6ec65cd2d6f408d534ead7d9d6e27523c8
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-17 11:18:09 +0200

    Add WalletDetailView to allow users to see their wallet and history

--- wallets/views.py
+++ wallets/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from wallets.models import *
+from wallets.serializers import *
+from rest_framework import generics, permissions
 
-# Create your views here.
+class WalletDetailView(generics.RetrieveAPIView):
+    """
+    API view for a user to retrieve his/her wallet details and transaction history.
+    """
+    serializer_class = WalletSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        # Expecting a query parameter "type" to determine which wallet to return.
+        wallet_type = self.request.query_params.get('type', 'rider')  # default to rider
+        # For boss wallet, there is only one; for rider or commissioner, use the linked user.
+        return Wallet.objects.filter(owner=self.request.user, wallet_type=wallet_type).first()
