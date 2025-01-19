bd420273afea52c95ab0a057f82844af689bc506
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-01-19 11:14:30 +0200

    Add transactions admin registration

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,18 @@
+from .models import *
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'rider', 'commissioner', 'boss',
+        'rider_total', 'commissioner_total', 'boss_total', 'created_at'
+    )
+    ordering = ('-created_at',)
+
+@admin.register(TransactionHistory)
+class TransactionHistoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'transaction', 'delivery_request',
+        'rider_amount', 'commissioner_amount', 'boss_amount', 'created_at'
+    )
+    ordering = ('-created_at',)
